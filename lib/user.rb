8d069090bb3ef3d2226f0ee4379cf71c48993d4a
Omar <omar.assed@gmail.com>
2022-07-04 10:37:35 -0600

    commit tree 005a11812dcbd4845b27b767814b0d61671d294f

---
+++ lib/user.rb
@@ -0,0 +1,8 @@
+require_relative "lib/teacher.rb"
+class User
+    attr_accessor :first_name, :last_name
+    def initialize first_name, last_name
+        @first_name = first_name
+        @last_name = last_names
+end
+
